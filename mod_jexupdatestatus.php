12e417a1880730a6223570786d9317662c2536e4
Unay Santisteban <davidu@softcom.com>
2019-02-19 21:41:01 +0100

    Added languages. Module done.

--- mod_jexupdatestatus.php
+++ mod_jexupdatestatus.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package otherCode.Joomla
  * @subpackage mod_jexupdatestatus
@@ -8,8 +9,66 @@
  */
 defined('_JEXEC') or die('Restricted access');
 
+$data = new JObject();
+$server_url = trim($params->get('serverurl', null));
+$count_extensions = $params->get('countextensions', 0);
+$show_type = $params->get('showtype', 0);
 
+try {
 
+    if (empty($server_url)) {
+        throw new \Exception('MOD_JEXUPDATESTATUS_EX_UNDEFINED_SERVER_URL', 404);
+    }
+
+    jimport('joomla.http.http');
+    jimport('joomla.http.transport.stream');
+
+    $options = new \Joomla\Registry\Registry();
+    $http = new JHttp($options, new JHttpTransportStream($options));
+
+    $response = $http->get($server_url);
+
+    switch ($response->code) {
+        case 200:
+
+            $xml = new SimpleXMLElement($response->body);
+            switch ($xml->getName()) {
+                case 'extensionset';
+                    $data->set('type', 'MOD_JEXUPDATESTATUS_TYPE_COLLECTION');
+                    break;
+                case 'updates';
+                    $data->set('type', 'MOD_JEXUPDATESTATUS_TYPE_EXTENSION');
+                    break;
+                default:
+                    throw new \Exception('MOD_JEXUPDATESTATUS_EX_UNDEFINED_SERVER_TYPE', 400);
+            }
+
+            $data->set('status', 'MOD_JEXUPDATESTATUS_STATUS_ONLINE');
+            $data->set('css.string', 'success');
+
+            if ($data->get('type') == 'MOD_JEXUPDATESTATUS_TYPE_COLLECTION' && $count_extensions == 1) {
+                $data->set('total.extensions', $xml->count());
+            }
+
+            break;
+        default:
+            $data->set('status', 'MOD_JEXUPDATESTATUS_STATUS_OFFLINE');
+            $data->set('css.string', 'danger');
+    }
+
+} catch (\Exception $e) {
+
+    switch ($e->getCode()) {
+        case 400:
+        case 404:
+            $data->set('status', 'MOD_JEXUPDATESTATUS_STATUS_UNDEFINED');
+            $data->set('css.string', 'default');
+            $data->set('error.message', $e->getMessage());
+        default:
+            $data->set('status', 'MOD_JEXUPDATESTATUS_STATUS_UNDEFINED');
+            $data->set('css.string', 'default');
+    }
+}
 
 $moduleclass_sfx = $params->get('moduleclass_sfx');
 $layout = $params->get('layout', 'default');
--- tmpl/default.php
+++ tmpl/default.php
@@ -8,3 +8,39 @@
  */
 defined('_JEXEC') or die('Restricted access');
 ?>
+
+<div class="jexupdatestatus <?php echo $moduleclass_sfx ?>">
+
+    <?php if ($show_errors == 1 && !empty($data->get('error.message'))): ?>
+        <div class="form-control">
+            <div class="alert alert-danger" role="alert">
+                <strong><?php echo JText::_('MOD_JEXUPDATESTATUS_ERROR_LABEL'); ?></strong>
+                <?php echo JText::_($data->get('error.message')); ?>
+            </div>
+        </div>
+    <?php endif; ?>
+
+    <div class="form-control">
+        <div class="jexupdate-info-status"><?php echo JText::_('MOD_JEXUPDATESTATUS_STATUS_LABEL'); ?>
+            <span class="label label-<?php echo $data->get('css.string'); ?>">
+                <?php echo JText::_($data->get('status')); ?></span>
+        </div>
+    </div>
+
+    <?php if ($show_type == 1 && !empty($data->get('type'))): ?>
+        <div class="form-control">
+            <div class="jexupdate-info-type"> <?php echo JText::_('MOD_JEXUPDATESTATUS_TYPE_LABEL'); ?>
+                <span><?php echo JText::_($data->get('type')); ?></span>
+            </div>
+        </div>
+    <?php endif; ?>
+
+    <?php if ($count_extensions == 1 && !empty($data->get('total.extensions'))): ?>
+        <div class="form-control">
+            <div class="jexupdate-info-count"><?php echo JText::_('MOD_JEXUPDATESTATUS_EXTENSIONS'); ?>
+                <span><?php echo $data->get('total.extensions'); ?></span>
+            </div>
+        </div>
+    <?php endif; ?>
+
+</div>
